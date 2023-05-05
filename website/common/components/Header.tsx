1ad59f8fb70cd7ab83b52bfcfc2a9ef7596ead7a
Joey Corry <jcorry.git@gmail.com>
2023-05-04 22:43:02 -0700

    Removes `PlayPauseButton`.

--- website/common/components/Header.tsx
+++ website/common/components/Header.tsx
@@ -3,20 +3,16 @@ import { memo } from 'react';
 import { ColorSchemeButton } from './ColorSchemeButton';
 import { GitHubLink } from './GitHubLink';
 import styles from './Header.module.css';
-import { PlayPauseButton } from './PlayPauseButton';
 
 function UnmemoizedHeader() {
     return (
         <header className={styles.header}>
             <ul>
                 <li>
-                    <PlayPauseButton />
-                </li>
-                <li>
-                    <ColorSchemeButton />
-                </li>
-                <li>
                     <GitHubLink />
+                </li>
+                <li className="color-scheme">
+                    <ColorSchemeButton />
                 </li>
             </ul>
         </header>
