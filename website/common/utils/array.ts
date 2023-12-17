35db739f690acfab34db788759a3ecdcdd808d2d
Joey Corry <jcorry.git@gmail.com>
2023-12-17 14:11:19 -0800

    Refine array utility method types.

--- website/common/utils/array.ts
+++ website/common/utils/array.ts
@@ -4,20 +4,29 @@ export const getArrayElementAtIndex = Object.prototype.hasOwnProperty.call(
     Array.prototype,
     'at'
 )
-    ? <T>(array: T[] | readonly T[], index: number) =>
-          Array.prototype.at.call<T[] | readonly T[], [number], T | undefined>(
-              array,
-              index
-          )
-    : <T>(array: T[] | readonly T[], index: number) =>
-          array[index < 0 ? array.length + index : index];
+    ? function getArrayElementAtIndex<T extends unknown[]>(
+          array: [...T] | readonly [...T],
+          index: number
+      ): T[number] | undefined {
+          return Array.prototype.at.call<
+              [...T] | readonly [...T],
+              [number],
+              T | undefined
+          >(array, index);
+      }
+    : function getArrayElementAtIndex<T extends unknown[]>(
+          array: [...T] | readonly [...T],
+          index: number
+      ): T[number] | undefined {
+          return array[index < 0 ? array.length + index : index];
+      };
 
 export function getRandomArrayElement<T>(array: T[] | readonly T[]) {
     return array[Math.floor(Math.random() * array.length)];
 }
 
-export function getArrayElementAtModuloReducedIndex<T>(
-    array: T[] | readonly T[],
+export function getArrayElementAtModuloReducedIndex<T extends unknown[]>(
+    array: [...T] | readonly [...T],
     index: number
 ) {
     return getArrayElementAtIndex(array, modulo(index, array.length));
