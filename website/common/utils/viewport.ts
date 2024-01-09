5255876f19d34f834b4215d2551ba707859f8492
Joey Corry <jcorry.git@gmail.com>
2024-01-08 22:58:31 -0800

    Add missing `Viewport` properties.

--- website/common/utils/viewport.ts
+++ website/common/utils/viewport.ts
@@ -1,18 +1,32 @@
 type Viewport = {
     devicePixelRatio: number;
     height: number;
+    physicalHeight: number;
+    physicalWidth: number;
     width: number;
 };
 
 function createViewport(): Viewport {
     if (import.meta.env.SSR) {
-        return { devicePixelRatio: 1, width: 0, height: 0 };
+        return {
+            devicePixelRatio: 1,
+            height: 0,
+            physicalHeight: 0,
+            physicalWidth: 0,
+            width: 0,
+        };
     }
 
+    const { devicePixelRatio, innerWidth: width, innerHeight: height } = window;
+    const physicalHeight = devicePixelRatio * height;
+    const physicalWidth = devicePixelRatio * width;
+
     return {
-        devicePixelRatio: window.devicePixelRatio,
-        width: window.innerWidth,
-        height: window.innerHeight,
+        devicePixelRatio,
+        height,
+        physicalHeight,
+        physicalWidth,
+        width,
     };
 }
 
